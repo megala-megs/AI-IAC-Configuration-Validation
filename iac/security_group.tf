f48570d34e87f0d0e83807a29c965d3397cda8d2
megala-megs <megalamadhavanm@gmail.com>
2026-09-21 17:08:52 +0530

    Add AWS security group for SSH access

---
+++ iac/security_group.tf
@@ -0,0 +1,11 @@
+resource "aws_security_group" "demo" {
+  name = "demo-security-group"
+
+  ingress {
+    description = "SSH access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
